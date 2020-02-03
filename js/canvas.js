566ce4f954076325e88f0f863d91ea58b57e17a5
s5unanow <s5unanow@gmail.com>
2020-02-03 11:48:11 +0200

    partially created intercollision Engine

--- js/canvas.js
+++ js/canvas.js
@@ -4,10 +4,10 @@ const ACTOR_TYPES = {
   CIRCLE: "circle",
   SQUARE: "square"
 };
-const SCALE = 30;
-const MAX_SIZE_DEVIATION = 0.4;
+const SCALE = 36;
+const MAX_SIZE_DEVIATION = 0.3;
 const MAX_VELOCITY_DEVIATION = 0.6;
-const SPEED = 2;
+const SPEED = 3;
 
 
 function halfChance() {
@@ -66,6 +66,10 @@ class Circle extends Figure {
     super(pos, velocity, color, type, number);
     this.radius = radius;
     this.area = Math.PI * radius ** 2;
+    this.center;
+  }
+  get center() {
+    return new Vec(this.pos.x, this.pos.y)
   }
 }
 
@@ -75,9 +79,12 @@ class Square extends Figure{
     this.side = side;
     this.area = side ** 2;
     this.center;
+    this.radiusInner = this.side / 2;
+    this.radiusOuter = Math.sqrt(this.radiusInner ** 2 + this.radiusInner ** 2);
+    this.radius = this.radiusInner + this.radiusOuter;
   }
   get center() {
-    return new Vec(this.pos.sum(this.side / 2));
+    return new Vec(this.pos.x + this.radiusInner, this.pos.y + this.radiusInner);
   }
 }
 
@@ -93,7 +100,7 @@ class ActorGenerator {
     let color = randRGBcolor();
     let pos = new Vec(1, 1);
     let velocity = Vec.randomVec().multiply(SPEED);
-    let side = SCALE;
+    let side = SCALE * randMultiplier(MAX_SIZE_DEVIATION);
     return new Square(pos, velocity, color, ACTOR_TYPES.SQUARE, number, side)
   }
   static generateActor(type, number) {
@@ -128,7 +135,7 @@ class Actors {
     }
   }
   logGeneratedActor(actor) {
-    console.log(`Number: ${actor.number}, Type: ${actor.type}, Color: ${actor.color}, Area: ${actor.area}`);
+    console.log(`Number: ${actor.number}, Type: ${actor.type}, Color: ${actor.color}, Area: ${actor.area.toFixed(1)}`);
   }
   runAutoAddFlow() {
     if (this.actors.length < this.maxSize) {
@@ -164,21 +171,52 @@ class Engine {
   }
   static wallCollisionCircle(circle, width, height) {
     if (circle.pos.x + circle.radius + circle.velocity.x > width ||
-        circle.pos.x - circle.radius + circle.velocity.x < 0) {
-          circle.velocity.x = -circle.velocity.x;
+    circle.pos.x - circle.radius + circle.velocity.x < 0) {
+      circle.velocity.x = -circle.velocity.x;
     }
     if (circle.pos.y + circle.radius + circle.velocity.y > height ||
-        circle.pos.y - circle.radius + circle.velocity.y < 0) {
-          circle.velocity.y = -circle.velocity.y;
+    circle.pos.y - circle.radius + circle.velocity.y < 0) {
+      circle.velocity.y = -circle.velocity.y;
     }
     circle.pos.x += circle.velocity.x;
     circle.pos.y += circle.velocity.y;
   }
   static wallCollisionSquare(square, width, height) {
-
+    if (square.pos.x + square.side + square.velocity.x > width ||
+    square.pos.x + square.velocity.x < 0) {
+      square.velocity.x = - square.velocity.x;
+    }
+    if (square.pos.y + square.side + square.velocity.y > height ||
+    square.pos.y + square.velocity.y < 0) {
+      square.velocity.y = - square.velocity.y;
+    }
+    square.pos.x += square.velocity.x;
+    square.pos.y += square.velocity.y;
   }
   static checkAndUpdateIfElementsCollide(actors) {
+    let collided = new Array(actors.length).fill(false);
+    let maxSafeDistance = SCALE * 2; //may be a problem with high speed; change "2" into SPEED?
 
+    actors.forEach((curActor, index) => {
+      if (!collided[index] && index !== actors.length - 1) {
+        for (let next = index + 1; next < actors.length; next++) {
+          //light check for the collision
+          if (Math.abs(curActor.center.x - next.center.x) > maxSafeDistance ||
+          Math.abs(curActor.center.y - next.center.y) > maxSafeDistance) {
+          // nothing here
+          } else if (curActor.radius + next.radius > calcDistance(curActor, next)) { //hard check for the collision
+            collided[index] = true;
+            collided[next] = true;
+
+          }
+        }
+      }
+    });
+
+    function calcDistance(actor1, actor2) {
+      return Math.sqrt(((actor1.center.x + actor1.velocity.x) - (actor2.center.x + actor2.velocity.x)) ** 2 +
+        ((actor1.center.y + actor1.velocity.y) - (actor2.center.y + actor2.velocity.y)) ** 2)
+    }
   }
 }
 
@@ -247,5 +285,38 @@ actors.addActor("circle");
 actors.addActor("circle");
 actors.addActor("circle");
 actors.addActor("circle");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
+actors.addActor("square");
 
 render.renderView();
+
+let arr = [1, 3, 5 ,7];
+
+arr.forEach((elem, index) => {
+  if (elem > 4) {
+    arr[index] = elem * 2;
+    return
+  }
+  if (elem > 2) {
+    arr[index] = elem * 6;
+  }
+});
+
+console.log(arr);
